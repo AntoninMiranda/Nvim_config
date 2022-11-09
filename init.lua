66c480df98cf26fa7fdde02b7584332a0d8acf1d
AntoninMiranda <antonin.miranda@epitech.eu>
2022-11-09 16:12:57 +0100

    deleted

--- init.lua
+++
@@ -1,5 +0,0 @@
-require('plugin')
-vim.cmd[[colorscheme gruvbox]]
-require('displays')
-require('nvim-autopairs').setup()
-require('cmp').event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
--- lua/plugin.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'gruvbox.nvim'      --colorthemes
-end)
-
